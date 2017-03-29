c7f858146a5f60d489c59803da071092e1aa5971
Sam Jenkins (Mizahnyx) <sjenkinsc@gmail.com>
2017-03-29 08:23:05 -0600

    Skeleton CSS, Servidor web de prueba

--- site/Gulpfile.js
+++ site/Gulpfile.js
@@ -1,7 +1,16 @@
 var gulp = require('gulp');
 var gulpPug = require('gulp-pug');
+var connect = require('gulp-connect');
+var gutil = require('gulp-util');
+var coffeescript = require('gulp-coffeescript');
 
 var TARGET_DIR = '../build/site';
+ 
+gulp.task('coffee', function() {
+    gulp.src(['**/*.coffee', '!node_modules/**/*.coffee'])
+        .pipe(coffeescript({bare: true}).on('error', gutil.log))
+        .pipe(gulp.dest(TARGET_DIR));
+});
 
 gulp.task('pug', function () {
     return gulp.src(['**/*.pug', '!node_modules/**/*.pug'])
@@ -10,7 +19,15 @@ gulp.task('pug', function () {
             pretty: true,
             basedir: './'
         }))
+        .on('error', gutil.log)
         .pipe(gulp.dest(TARGET_DIR));
 });
 
-gulp.task('default', ['pug']);
+gulp.task('connect', function() {
+    connect.server({
+        root: TARGET_DIR,
+        livereload: true
+    });
+});
+
+gulp.task('default', ['coffee', 'pug']);
